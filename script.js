c2632e643a90843ce6ccda65afe0d106ffe9eb0d
Abhiram302 <abhiramburra3@gmail.com>
2025-07-30 23:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+
+const change = document.getElementById("change_button")
+const reset = document.getElementById("Reset")
+change.addEventListener("click",()=>{
+	for(let i=1; i<=9; i++){
+		document.getElementById(i.toString()).style.backgroundColor = "transparent"
+	}
+	
+	const num = document.getElementById("block_id").value
+const colorr = document.getElementById("colour_id").value
+	const box = document.getElementById(num.toString())
+	box.style.backgroundColor = colorr;
+	
+})
+
+reset.addEventListener("click",()=>{
+	for(let i=1; i<=9; i++){
+		document.getElementById(i.toString()).style.backgroundColor = "transparent"
+	}
+})
